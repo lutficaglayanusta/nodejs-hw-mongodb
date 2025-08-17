1ed56ef8d622620cada84f38a0de5b20d9197d0e
caglayan <caglayanustaa@gmail.com>
2025-08-18 00:43:15 +0300

    nodejs goit second project

---
+++ src/server.js
@@ -0,0 +1,49 @@
+import express from "express";
+import pino from "pino-http";
+import cors from "cors";
+import { env } from "./utils/env.js";
+
+const PORT = Number(env('PORT','3000'));
+
+export const setupServer = () => {
+    const app = express();
+
+    app.use(
+      pino({
+        transport: {
+          target: "pino-pretty",
+        },
+      })
+    );
+
+
+
+    app.use(express.json());
+    app.use(cors());
+
+
+    app.get("/", (req, res) => {
+        res.status(200).json({
+            message: "Home page sayfası"
+        });
+    });
+
+    app.use((req, res, next) => {
+      res.status(404).json({
+        message: "Not found",
+      });
+    });
+
+
+    app.use((err, req, res, next) => {
+      res.status(500).json({
+        message: "Something went wrong",
+        error: err.message,
+      });
+    });
+
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+};
