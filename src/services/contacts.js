4a4fe309758e24a32eb549bbc0e86b951aaab309
caglayan <caglayanustaa@gmail.com>
2025-08-26 14:40:55 +0300

    goit nodejs third project done

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -8,3 +8,30 @@ export const getContactId = async (contactId) => {
   const contact = await Contacts.findById(contactId);
   return contact;
 };
+export const createContact = async (payload) => {
+  const contact = await Contacts.create(payload);
+  return contact;
+};
+export const updateContact = async (contactId, payload, options = {}) => {
+  const contactResult = await Contacts.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    }
+  );
+
+  if (!contactResult || !contactResult.value) {
+    return null;
+  }
+  return {
+    contact: contactResult.value,
+    isNew: Boolean(contactResult?.lastErrorObject?.upserted),
+  };
+};
+export const deleteContact = async (contactId) => {
+  const contact = await Contacts.findOneAndDelete({ _id: contactId });
+  return contact;
+};
